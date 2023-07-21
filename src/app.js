c06b4e16d757c9f4968eb6f7a5e4ae6f44297c59
DanielGroelle <DanielGroelle@protonmail.com>
2023-07-21 02:37:09 -0400

    accomodate for cors, probably

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 if (process.env.USER) require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const app = express();
 
 const notFound = require("./errors/notFound");
@@ -8,6 +9,9 @@ const unknownError = require("./errors/unknownError");
 const moviesRouter = require("./movies/movies.router");
 const reviewsRouter = require("./reviews/reviews.router");
 const theatersRouter = require("./theaters/theaters.router");
+
+//allows for cross origin requests for every route
+app.use(cors());
 
 //allows us to send back json data
 app.use(express.json());
